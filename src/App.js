79e279736de31bac078e817ded62c8bea3b13cae
Shauna Griffin <shaunahennessygriffin@gmail.com>
2020-04-27 13:22:17 +0200

    Initial commit and setup of portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import float from "./assets/float.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1 className="section--header">My React Portfolio</h1>
+        <img src={logo} className="App-logo" alt="react symbol" />
+      </nav>
+      <main>
+        <section className="section--about-me">
+          <div className="text--wrapper">
+            <h2 className="font--secondary">Hey, I'm ReDi.</h2>
+            <p>
+              Here's the section where I describe how awesome it is to work with
+              me. I am really into these technologies and this is what I bring
+              to the table. When I'm not coding you can find me going for runs
+              with my dog or drinking way too much coffee.
+            </p>
+          </div>
+          <div className="image--wrapper">
+            <img className="image" src={float} alt="woman floating" />
+          </div>
+        </section>
+        <section>
+          <h3 className="section--header">My work</h3>
+        </section>
+        <section>
+          <h3 className="section--header">Skills</h3>
+        </section>
+      </main>
     </div>
   );
 }
